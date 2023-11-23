12d366d349f338d39930e3814f2f8b78af380ce0
Shark-PT <jccreis@hotmail.com>
2023-11-23 13:43:56 +0000

    Correção de Bugs

--- main.py
+++ main.py
@@ -14,29 +14,32 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+
 def start_timer():
     count_down(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+
 def count_down(count):
 
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down,count -1)
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
-
-
-timer_label = Label(text="Timer", bg=YELLOW,fg=GREEN, font=(FONT_NAME, 35, "bold"))
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
 timer_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -51,11 +54,7 @@ start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
-checkmark_label = Label(text="✔", bg=YELLOW,fg=GREEN)
+checkmark_label = Label(text="✔", bg=YELLOW, fg=GREEN)
 checkmark_label.grid(column=1, row=3)
-
-
-
-
 
 window.mainloop()
